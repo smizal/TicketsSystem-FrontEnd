945bab9046896a70068b922680710205570176c3
Loay Al-Mugharghar <almughargharloay@gmail.com>
2025-01-07 19:36:06 +0300

    Register form, with functionalities done.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,206 +1,259 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Register = () => {
+  const initCompanyForm = {
+    companyName: "",
+    companyEmail: "",
+    phone: "",
+    address: "",
+    logo: "",
+    adminName: "",
+    adminEmail: "",
+    cpr: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  }
+
+  const [companyForm, setCompanyForm] = useState(initCompanyForm)
+
+  const handleChange = (event) => {
+    console.log(event.target.id, event.target.value)
+    console.log(companyForm)
+
+    setCompanyForm({ ...companyForm, [event.target.id]: event.target.value })
+    console.log(companyForm)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(companyForm)
+    setCompany(initCompanyForm)
+    useNavigate("/login")
+  }
   return (
-    <div class="container-register m-auto text-left">
+    <div className="container-register m-auto text-left">
       <main>
-        <div class="py-5 text-center">
+        <div className="py-5 text-center">
           <img
-            class="d-block mx-auto mb-4"
+            className="d-block mx-auto mb-4"
             src="../assets/brand/bootstrap-logo.svg"
             alt=""
             width="72"
             height="57"
           />
           <h2>Register a company</h2>
-          <p class="lead">
+          <p className="lead">
             Submit a request to review and approve by filling the below form
             built entirely for easy submit and fast review/approve by submitting
             it, you are approving all the data and it's validity.
           </p>
         </div>
 
-        <div class="row g-5 text-start">
-          <div class="col-md-12 col-lg-12">
-            <h4 class="mb-3">Company Details</h4>
-            <form class="needs-validation" novalidate>
-              <div class="row g-3">
-                <div class="col-sm-12">
-                  <label for="CompanyName" class="form-label">
+        <div className="row g-5 text-start">
+          <div className="col-md-12 col-lg-12">
+            <h4 className="mb-3">Company Details</h4>
+            <form className="needs-validation" novalidate>
+              <div className="row g-3">
+                <div className="col-sm-12">
+                  <label for="companyName" className="form-label">
                     Company name
                   </label>
                   <input
                     type="text"
-                    class="form-control"
-                    id="name"
+                    className="form-control"
+                    id="companyName"
                     placeholder=""
-                    value=""
+                    value={companyForm.companyName}
                     required
+                    onChange={handleChange}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Valid Company name is required.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="companyEmail" class="form-label">
+                <div className="col-12">
+                  <label for="companyEmail" className="form-label">
                     Email
                   </label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="companyEmail"
                     placeholder="you@example.com"
+                    onChange={handleChange}
+                    value={companyForm.companyEmail}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter a valid email address for shipping updates.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="phone" class="form-label">
+                <div className="col-12">
+                  <label for="phone" className="form-label">
                     Phone
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="phone"
                     placeholder="+973-12345678"
+                    onChange={handleChange}
+                    value={companyForm.phone}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter a valid phone number for receiving further
                     updates.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="address" class="form-label">
+                <div className="col-12">
+                  <label for="address" className="form-label">
                     Address
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="address"
                     placeholder="1234 Main St"
                     required
+                    onChange={handleChange}
+                    value={companyForm.address}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter your company address.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="logo" class="form-label">
+                <div className="col-12">
+                  <label for="logo" className="form-label">
                     Logo
                   </label>
                   <input
                     type="file"
-                    class="form-control"
+                    className="form-control"
                     id="photo"
                     placeholder="logo"
+                    onChange={handleChange}
+                    value={companyForm.logo}
                   />
                 </div>
               </div>
 
-              <hr class="my-4" />
-              <h4 class="mb-3">Admin User Details</h4>
-              <div class="row g-3">
-                <div class="col-sm-12">
-                  <label for="adminName" class="form-label">
+              <hr className="my-4" />
+              <h4 className="mb-3">Admin User Details</h4>
+              <div className="row g-3">
+                <div className="col-sm-12">
+                  <label for="adminName" className="form-label">
                     Admin Name
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="adminName"
                     placeholder=""
-                    value=""
+                    value={companyForm.adminName}
                     required
+                    onChange={handleChange}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Valid Company name is required.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="adminEmail" class="form-label">
+                <div className="col-12">
+                  <label for="adminEmail" className="form-label">
                     Email
                   </label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="adminEmail"
                     placeholder="you@example.com"
+                    onChange={handleChange}
+                    value={companyForm.adminEmail}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter a valid email address for shipping updates.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="cpr" class="form-label">
+                <div className="col-12">
+                  <label for="cpr" className="form-label">
                     CPR
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="cpr"
                     placeholder="CPR"
+                    onChange={handleChange}
+                    value={companyForm.cpr}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter a valid username.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="username" class="form-label">
+                <div className="col-12">
+                  <label for="username" className="form-label">
                     Username
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="username"
                     placeholder="Username"
+                    onChange={handleChange}
+                    value={companyForm.username}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter a valid username.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="password" class="form-label">
+                <div className="col-12">
+                  <label for="password" className="form-label">
                     Password
                   </label>
                   <input
-                    type="text"
-                    class="form-control"
+                    type="password"
+                    className="form-control"
                     id="password"
                     placeholder="********"
                     required
+                    onChange={handleChange}
+                    value={companyForm.password}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter your password.
                   </div>
                 </div>
 
-                <div class="col-12">
-                  <label for="confirmPassword" class="form-label">
+                <div className="col-12">
+                  <label for="confirmPassword" className="form-label">
                     Confirm Password
                   </label>
                   <input
-                    type="text"
-                    class="form-control"
+                    type="password"
+                    className="form-control"
                     id="confirmPassword"
                     placeholder="********"
                     required
+                    onChange={handleChange}
+                    value={companyForm.confirmPassword}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please enter your confirm password.
                   </div>
                 </div>
               </div>
 
-              <hr class="my-4" />
+              <hr className="my-4" />
 
-              <button class="w-100 btn btn-primary btn-lg" type="submit">
+              <button className="w-100 btn btn-primary btn-lg" type="submit">
                 Submit register request
               </button>
             </form>
