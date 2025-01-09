adbd2c06e6c4a7917a4c40a58b57d9c64cfd48a2
Loay Al-Mugharghar <almughargharloay@gmail.com>
2025-01-09 21:20:01 +0300

    Loay: Login Functionalities done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,71 @@
 import logoSvg from "../components/logo.svg"
-const Login = () => {
-  return (
-    <div class="form-signin w-100 m-auto">
-      <form onSubmit>
-        <img class="mb-4" src={logoSvg} alt="" width="72" height="57" />
-        <h1 class="h3 mb-3 fw-normal">Welcome Back 👋🏻</h1>
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { signIn } from "../services/authService"
 
-        <div class="form-floating">
+const initialFormData = {
+  username: "",
+  password: "",
+}
+
+const Login = () => {
+  const navigate = useNavigate()
+  const [message, setMessage] = useState("")
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      const response = await signIn(formData)
+      console.log(response)
+
+      if (response.token) {
+        setMessage("Login successful")
+        setFormData(initialFormData)
+        navigate("/tickets-list")
+      } else {
+        setMessage("Login failed")
+      }
+    } catch (error) {
+      console.log(error)
+      setMessage("There is an error")
+    }
+  }
+
+  return (
+    <div className="form-signin w-100 m-auto">
+      <form onSubmit={handleSubmit}>
+        <img className="mb-4" src={logoSvg} alt="" width="72" height="57" />
+        <h1 className="h3 mb-3 fw-normal">Welcome Back 👋🏻</h1>
+        <div className="text-danger">{message}</div>
+        <div className="form-floating">
           <input
             type="username"
-            class="form-control"
-            id="floatingInput"
+            className="form-control"
+            id="username"
             placeholder="username / CPR"
+            onChange={handleChange}
+            value={formData.username}
           />
           <label for="username">Username</label>
         </div>
-        <div class="form-floating">
+        <div className="form-floating">
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="password"
             placeholder="password"
+            onChange={handleChange}
+            value={formData.password}
           />
           <label for="password">Password</label>
         </div>
 
-        <button
-          class="btn btn-primary w-100 py-2"
-          id="submitButton"
-          type="submit"
-        >
+        <button className="btn btn-primary w-100 py-2" id="submitButton">
           Sign in
         </button>
       </form>
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,22 @@
-import client from './config'
+import client from "./config"
 
 export const signUp = async (data) => {
-  const response = await client.post('/auth/signup', data)
+  const response = await client.post("/auth/signup", data)
   const token = response.data.token
-  localStorage.setItem('authToken', token)
+  localStorage.setItem("authToken", token)
   return response.data
 }
 
 export const signIn = async (data) => {
-  const response = await client.post('/auth/signin', data)
-  const token = response.data.token
-  localStorage.setItem('authToken', token)
-  return response.data
+  try {
+    const response = await client.post("/auth/login", data)
+    if (response.data.error) {
+      return response.data.error
+    }
+    const token = response.data.token
+    localStorage.setItem("authToken", token)
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
 }
