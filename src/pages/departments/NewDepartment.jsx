500459155e83ae41f25838ac9f5d1598f96de14f
smizal <sjs29@hotmail.com>
2025-01-10 20:50:24 +0300

    Salman: New Department functionality created

--- src/pages/departments/NewDepartment.jsx
+++ src/pages/departments/NewDepartment.jsx
@@ -1,14 +1,106 @@
-const NewDepartment = () => {
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import departmentsService from '../../services/departmentsService'
+import companiesService from '../../services/companiesService'
+
+const initialFormData = {
+  companyId: '',
+  name: '',
+  description: '',
+  status: 'active'
+}
+
+const NewDepartment = ({ user }) => {
+  const navigate = useNavigate()
+  const [message, setMessage] = useState('')
+  const [formData, setFormData] = useState(initialFormData)
+  const [companies, setCompanies] = useState(null)
+
+  useEffect(() => {
+    const companiesList = async () => {
+      const data = await companiesService.index()
+      setCompanies(data)
+      formData.companyId = data[0]._id
+    }
+    companiesList()
+  }, [])
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      setMessage('')
+      const data = await departmentsService.create(formData)
+
+      if (data) {
+        if (data.error) {
+          setMessage({ msg: data.error, type: 'alert alert-danger' })
+        } else {
+          setMessage({
+            msg: 'Department Added Successfully',
+            type: 'alert alert-success'
+          })
+          setFormData(initialFormData)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+      setMessage({
+        msg: 'There is an error, please contact the administrator',
+        type: 'alert alert-danger'
+      })
+    }
+  }
+
+  const isFormInvalid = () => {
+    return !(
+      formData.companyId &&
+      formData.name &&
+      formData.description &&
+      formData.status
+    )
+  }
+
   return (
     <>
       <div className="container">
         <h1 className="mt-5">New Department</h1>
+        {message ? <div className={message.type}>{message.msg}</div> : null}
         <div
           className="form-signin mx-auto"
-          style={{ maxWidth: "600px", padding: "20px" }}
+          style={{ maxWidth: '600px', padding: '20px' }}
         >
-          <form onSubmit="">
-            <input type="hidden" id="source" name="source" value="web" />
+          <form onSubmit={handleSubmit}>
+            {user.role === 'admin' ? (
+              <input
+                type="hidden"
+                id="companyId"
+                name="companyId"
+                value={user.companyId}
+              />
+            ) : (
+              <div className="form-floating mb-3">
+                <div className="form-floating mb-3">
+                  {companies ? (
+                    <select
+                      className="form-control"
+                      id="companyId"
+                      placeholder="Company Name"
+                      onChange={handleChange}
+                      defaultValue={companies[0]._id}
+                      required
+                    >
+                      {companies.map((company, index) => (
+                        <option value={company._id}>{company.name}</option>
+                      ))}
+                    </select>
+                  ) : null}
+                </div>
+              </div>
+            )}
 
             <div className="form-floating mb-3">
               <input
@@ -16,8 +108,8 @@ const NewDepartment = () => {
                 className="form-control"
                 id="name"
                 placeholder="Name"
-                // onChange=""
-                // value={ticketForm.name}
+                onChange={handleChange}
+                value={formData.name}
                 required
               />
               <label htmlFor="name">Name</label>
@@ -25,15 +117,15 @@ const NewDepartment = () => {
 
             <div className="form-floating mb-3">
               <input
-                type="email"
+                type="text"
                 className="form-control"
-                id="email"
+                id="description"
                 placeholder="Description"
-                // onChange={handleChange}
-                // value={ticketForm.email}
+                onChange={handleChange}
+                value={formData.description}
                 required
               />
-              <label htmlFor="email">Description</label>
+              <label htmlFor="description">Description</label>
             </div>
 
             <div className="form-floating mb-3">
@@ -42,42 +134,20 @@ const NewDepartment = () => {
                 className="form-control"
                 id="phone"
                 placeholder="Phone"
-                // onChange={handleChange}
-                // value={ticketForm.phone}
+                onChange={handleChange}
+                defaultValue={formData.status}
                 required
-              />
+              >
+                <option value="active">Active</option>
+                <option value="suspended">Suspended</option>
+              </select>
               <label htmlFor="phone">Status</label>
             </div>
-
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="notes"
-                placeholder="Enter your CPR"
-                // onChange={handleChange}
-                // value={ticketForm.cpr}
-                required
-              />
-              <label htmlFor="notes">Notes</label>
-            </div>
-
-            <div className="form-floating mb-3">
-              <div className="form-floating mb-3">
-                <select
-                  className="form-control"
-                  id="companyName"
-                  // placeholder="Company Name"
-                  // onChange={handleChange}
-                  // value={ticketForm.phone}
-                  required
-                />
-                <label htmlFor="companyName">Company Name</label>
-              </div>
-              {/* <label htmlFor="companyName">Company Name</label> */}
-            </div>
-            <button className="btn btn-primary w-100 py-2" type="submit">
-              Submit
+            <button
+              className="btn btn-primary w-100 py-2"
+              disabled={isFormInvalid}
+            >
+              Add Department
             </button>
           </form>
         </div>
--- src/services/companiesService.js
+++ src/services/companiesService.js
@@ -2,7 +2,7 @@ import client from './config'
 
 const index = async () => {
   try {
-    const response = await client.get('/departments')
+    const response = await client.get('/companies')
     return response.data
   } catch (error) {
     console.log(error)
@@ -10,33 +10,33 @@ const index = async () => {
 }
 
 const deleting = async (id) => {
-  try {
+  /*  try {
     const response = await client.delete(`/departments/${id}`)
     console.log(response.data)
     return response.data
   } catch (error) {
     console.log(error)
-  }
+  } */
 }
 
 const update = async (id, data) => {
-  try {
+  /* try {
     const response = await client.put(`/departments/edit/${id}`, data)
     console.log(response.data)
     return response.data
   } catch (error) {
     console.log(error)
-  }
+  } */
 }
 
-const create = async (data) => {
-  try {
+const create = async (id, data) => {
+  /* try {
     const response = await client.post(`/departments/`, data)
     console.log(response.data)
     return response.data
   } catch (error) {
     console.log(error)
-  }
+  } */
 }
 
 export default { index, deleting, update, create }
