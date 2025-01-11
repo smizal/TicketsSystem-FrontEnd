164366bf12191e6552c0f4f4993cd3f53ea48753
GARajab <mohamed.rajab@ewa.bh>
2025-01-12 00:15:15 +0300

    loading spining added to depList

--- src/pages/departments/DepartmentsList.jsx
+++ src/pages/departments/DepartmentsList.jsx
@@ -1,80 +1,61 @@
-import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import departmentsService from '../../services/departmentsService'
-import DepartmentRow from './DepartmentRow'
-import DepartmentRows from './DepartmentsRows'
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import departmentsService from "../../services/departmentsService"
+import DepartmentRow from "./DepartmentRow"
 
 const DepartmentsList = () => {
+
   const navigate = useNavigate()
   const [message, setMessage] = useState(null)
   const [departments, setDepartments] = useState(null)
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true)
 
-  try {
-    const getList = async () => {
-      setLoading(true);
+  const getList = async () => {
+    setLoading(true)
+    const data = await departmentsService.index()
+    setDepartments(data ? (data.length > 0 ? data : null) : null)
+    setLoading(false)
+  }
+
+  const handleDelete = async (e) => {
+    setLoading(true)
+    const data = await departmentsService.deleting(e.target.id)
+    if (data) {
+      if (data.error) {
+        setMessage({ msg: data.error, type: "alert alert-danger" })
+      } else {
+        setMessage({ msg: data.message, type: "alert alert-info" })
+      }
+      getList()
+      setLoading(false)
+    }
+  }
+
+  const handleStatus = async (e) => {
+    setLoading(true)
+    const data = await departmentsService.update(e.target.id, {
+      status: e.target.name,
+    })
+    if (data) {
+      if (data.error) {
+        setMessage({ msg: data.error, type: "alert alert-danger" })
+      } else {
+        setMessage({ msg: data.message, type: "alert alert-info" })
+      }
+      getList()
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    const fetchDefaultList = async () => {
       const data = await departmentsService.index()
-      console.log(data)
-      setDepartments(setDepartments(data ? (data.length > 0 ? data : []) : []))
+      setDepartments(data)
+      setLoading(false)
     }
-  } catch (error) {
-    setLoading(false);
-    console.log(error)
-
-  }
-
-  try {
-    setLoading(true)
-    const handleDelete = async (e) => {
-      const data = await departmentsService.deleting(e.target.id)
-      if (data) {
-        if (data.error) {
-          setMessage({ msg: data.error, type: 'alert alert-danger' })
-        } else {
-          setMessage({ msg: data.message, type: 'alert alert-info' })
-        }
-        getList()
-      }
-    }
-  } catch (error) {
-    setLoading(false)
-    console.log(error)
-  }
-
-  try {
-    setLoading(true)
-    const handleStatus = async (e) => {
-      const data = await departmentsService.update(e.target.id, {
-        status: e.target.name
-      })
-      if (data) {
-        if (data.error) {
-          setMessage({ msg: data.error, type: 'alert alert-danger' })
-        } else {
-          setMessage({ msg: data.message, type: 'alert alert-info' })
-        }
-        getList()
-      }
-    }
-  } catch (error) {
-    setLoading(false)
-    console.log(error)
-  }
-
-  try {
-    setLoading(true)
-    useEffect(() => {
-      const fetchDefaultList = async () => {
-        const data = await departmentsService.index()
-        console.log(data)
-        setDepartments(setDepartments(data ? (data.length > 0 ? data : []) : []))
-      }
-      fetchDefaultList()
-    }, [])
-  } catch (error) {
-    setLoading(false)
-    console.log(error)
-  }
+    fetchDefaultList()
+  }, [])
 
   return (
     <>
@@ -90,7 +71,46 @@ const DepartmentsList = () => {
             </Link>
           </div>
           {message ? <div className={message.type}>{message.msg}</div> : null}
-          <DepartmentRows departments={DepartmentRows} getList={getList} handleDelete={handleDelete} handleStatus={handleStatus} />
+          {loading ? (
+            <div className="text-center">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          ) : departments ? (
+            <table className="table table-bordered table-hover text-center">
+              <thead className="thead-dark">
+                <tr>
+                  <th>ID</th>
+                  <th>Company</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody id="servicesTableBody">
+                {departments.length > 0 ? (
+                  departments.map((department, index) => (
+                    <DepartmentRow
+                      key={department._id}
+                      department={department}
+                      index={index}
+                      getList={getList}
+                      handleDelete={handleDelete}
+                      handleStatus={handleStatus}
+                    />
+                  ))
+                ) : (
+                  <tr>
+                    <td colspan="6">No departments found</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          ) : (
+            "No departments found"
+          )}
         </div>
       </div>
     </>
